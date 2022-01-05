7a9f8e7274aa7083c0b65fd10fd03c0769b4951d
Benjamin E. Iserman <winggar1228@gmail.com>
2022-01-05 00:44:29 -0600

    add unit tests for Get, Get<T>, GetCheck, GetCheck<T>, GetYN, and GetYNStrict

---
+++ TestProject/Choice_Tests.cs
@@ -0,0 +1,79 @@
+﻿namespace TestProject;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using InputHandler;
+
+[TestClass]
+public class Choice_Tests
+{
+	[DataTestMethod]
+	[DataRow(0)]
+	[DataRow(1)]
+	[DataRow(2)]
+	[DataRow(5)]
+	[DataRow(10)]
+	[DataRow(100)]
+	public void GetYN(int n)
+	{
+		Queue<string> queue = new();
+		for (int i = 0; i < n; i++) queue.Enqueue("hi");
+		queue.Enqueue("y");
+
+		Assert.AreEqual(Input.GetYN(getString: () => queue.Dequeue()), true);
+
+		queue = new();
+		for (int i = 0; i < n; i++) queue.Enqueue("hi");
+		queue.Enqueue("yes");
+
+		Assert.AreEqual(Input.GetYN(getString: () => queue.Dequeue()), true);
+
+		queue = new();
+		for (int i = 0; i < n; i++) queue.Enqueue("hi");
+		queue.Enqueue("n");
+
+		Assert.AreEqual(Input.GetYN(getString: () => queue.Dequeue()), false);
+
+		queue = new();
+		for (int i = 0; i < n; i++) queue.Enqueue("hi");
+		queue.Enqueue("no");
+
+		Assert.AreEqual(Input.GetYN(getString: () => queue.Dequeue()), false);
+	}
+
+	[DataTestMethod]
+	[DataRow(0)]
+	[DataRow(1)]
+	[DataRow(2)]
+	[DataRow(5)]
+	[DataRow(10)]
+	[DataRow(100)]
+	public void GetYNStrict(int n)
+	{
+		Queue<string> queue = new();
+		for (int i = 0; i < n; i++) queue.Enqueue("hi");
+		queue.Enqueue("y");
+
+		Assert.AreEqual(Input.GetYNStrict(getString: () => queue.Dequeue()), true);
+
+		queue = new();
+		for (int i = 0; i < n; i++) queue.Enqueue("hi");
+		queue.Enqueue("yes");
+		queue.Enqueue("n");
+
+		Assert.AreEqual(Input.GetYNStrict(getString: () => queue.Dequeue()), false);
+
+		queue = new();
+		for (int i = 0; i < n; i++) queue.Enqueue("hi");
+		queue.Enqueue("n");
+
+		Assert.AreEqual(Input.GetYNStrict(getString: () => queue.Dequeue()), false);
+
+		queue = new();
+		for (int i = 0; i < n; i++) queue.Enqueue("hi");
+		queue.Enqueue("no");
+		queue.Enqueue("y");
+
+		Assert.AreEqual(Input.GetYNStrict(getString: () => queue.Dequeue()), true);
+	}
+}
---
+++ TestProject/Get_Tests.cs
@@ -0,0 +1,123 @@
+namespace TestProject;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using InputHandler;
+
+[TestClass]
+public class Get_Tests
+{
+	[DataTestMethod]
+	[DataRow(0)]
+	[DataRow(1)]
+	[DataRow(2)]
+	[DataRow(5)]
+	[DataRow(10)]
+	[DataRow(100)]
+	public void Get(int n)
+	{
+		Queue<string> queue = new();
+		for (int i = 0; i < n; i++) queue.Enqueue("hi");
+		queue.Enqueue("7.5");
+
+		double x = 0;
+
+		Input.Get(s => x = double.Parse(s), getString: () => queue.Dequeue());
+
+		Assert.AreEqual(x, 7.5);
+	}
+
+	[DataTestMethod]
+	[DataRow(0)]
+	[DataRow(1)]
+	[DataRow(2)]
+	[DataRow(5)]
+	[DataRow(10)]
+	[DataRow(100)]
+	public void GetT(int n)
+	{
+		Queue<string> queue = new();
+		for (int i = 0; i < n; i++) queue.Enqueue("hi");
+		queue.Enqueue("7.5");
+
+		double x = Input.Get(s => double.Parse(s), getString: () => queue.Dequeue());
+
+		Assert.AreEqual(x, 7.5);
+	}
+
+	[DataTestMethod]
+	[DataRow(0)]
+	[DataRow(1)]
+	[DataRow(2)]
+	[DataRow(5)]
+	[DataRow(10)]
+	[DataRow(100)]
+	public void GetCheck(int n)
+	{
+		Queue<string> queue = new();
+		for (int i = 0; i < n; i++) queue.Enqueue("hi");
+		queue.Enqueue("7.5");
+
+		double x = double.Parse(Input.GetCheck(s => double.TryParse(s, out _), getString: () => queue.Dequeue()));
+
+		Assert.AreEqual(x, 7.5);
+	}
+
+	[DataTestMethod]
+	[DataRow(0)]
+	[DataRow(1)]
+	[DataRow(2)]
+	[DataRow(5)]
+	[DataRow(10)]
+	[DataRow(100)]
+	public void GetTCheck(int n)
+	{
+		double x;
+		Queue<string> queue = new();
+		
+		for (int i = 0; i < n; i++) queue.Enqueue("hi");
+		queue.Enqueue("7.5");
+
+		x = Input.GetCheck(s => double.Parse(s), x => x < 10, getString: () => queue.Dequeue());
+
+		Assert.AreEqual(x, 7.5, "I");
+
+		for (int i = 0; i < n; i++) queue.Enqueue("12.5");
+		queue.Enqueue("7.5");
+
+		x = Input.GetCheck(s => double.Parse(s), x => x < 10, getString: () => queue.Dequeue());
+
+		Assert.AreEqual(x, 7.5, "II");
+
+		for (int i = 0; i < n; i++)
+		{
+			queue.Enqueue("hi");
+			queue.Enqueue("12.5");
+		}
+
+		queue.Enqueue("7.5");
+
+		x = Input.GetCheck(s => double.Parse(s), x => x < 10, getString: () => queue.Dequeue());
+
+		Assert.AreEqual(x, 7.5, "III");
+
+		for (int i = 0; i < n; i++)
+		{
+			queue.Enqueue("12.5");
+			queue.Enqueue("hi");
+		}
+
+		queue.Enqueue("7.5");
+
+		x = Input.GetCheck(s => double.Parse(s), x => x < 10, getString: () => queue.Dequeue());
+
+		Assert.AreEqual(x, 7.5, "IV");
+	}
+
+	[TestMethod]
+	public void GetBool()
+	{
+		Assert.AreEqual(Input.GetBool(s => char.IsUpper(s[0]), () => "hi"), false);
+		Assert.AreEqual(Input.GetBool(s => char.IsUpper(s[0]), () => "Hi"), true);
+	}
+}
