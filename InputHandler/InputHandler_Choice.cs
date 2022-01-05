feab77d130885ddd48134e532aae16b365458c21
Benjamin E. Iserman <winggar1228@gmail.com>
2022-01-05 00:16:33 -0600

    Add project files.

---
+++ InputHandler/InputHandler_Choice.cs
@@ -0,0 +1,151 @@
+﻿namespace InputHandler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static partial class Input
+{
+    /// <summary>
+    /// Prompts for input until it receives y, n, yes, or no (not case sensitive). Can optionally use parameter <paramref name="message"/> to specify an error message.
+    /// </summary>
+    /// <param name="getString">The function used to get user input. Defaults to Console.ReadLine. (optional)</param>
+    /// <param name="write">The function used to output errors. Defaults to Console.WriteLine. (optional)</param>
+    /// <param name="message">The error message sent if something other than y, n, yes, or no is inputted. (optional)</param>
+    /// <returns>Once a valid input is entered: <see langword="true"/> if input was "y" or "yes", <see langword="false"/> if input was "n" or "no".</returns>
+    public static bool GetYN(Func<string> getString = null, Action<string> write = null, string message = "Invalid format... use y or n.")
+    {
+        getString ??= Console.ReadLine;
+        write ??= Console.WriteLine;
+
+        while (true)
+        {
+            switch (getString().Trim().ToLower())
+            {
+                case "y":
+                case "yes":
+                    return true;
+                case "n":
+                case "no":
+                    return false;
+                default:
+                    write(message);
+                    continue;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Prompts for input using until it receives a string contained in <paramref name="yesOptions"/> or <paramref name="noOptions"/>. Can optionally use parameter <paramref name="message"/> to specify an error message.
+    /// </summary>
+    /// <param name="yesOptions">A collection of strings for which the method should return true.</param>
+    /// <param name="noOptions">A collection of strings for which the method should return false.</param>
+    /// <param name="getString">The function used to get user input. Defaults to Console.ReadLine. (optional)</param>
+    /// <param name="write">The function used to output errors. Defaults to Console.WriteLine. (optional)</param>
+    /// <param name="message">The error message sent if a string contained in neither <paramref name="yesOptions"/> nor <paramref name="noOptions"/> is inputted. (optional)</param>
+    /// <returns>Once a valid input is entered: <see langword="true"/> if <paramref name="yesOptions"/> contains input, <see langword="false"/> if <paramref name="noOptions"/> contains input. In the case that both <paramref name="yesOptions"/> and <paramref name="noOptions"/> contain input, returns <see langword="true"/>.</returns>
+    public static bool GetYN(IEnumerable<string> yesOptions, IEnumerable<string> noOptions, Func<string> getString = null, Action<string> write = null, string message = "Specified input is not a valid yes or no option. Please try a different input.")
+    {
+        getString ??= Console.ReadLine;
+        write ??= Console.WriteLine;
+
+        while (true)
+        {
+            string s = getString();
+            if (yesOptions.Contains(s)) return true;
+            else if (noOptions.Contains(s)) return false;
+            else write(message);
+        }
+    }
+
+    /// <summary>
+    /// Prompts for input using until it receives y or n (not case sensitive). Can optionally use parameter <paramref name="message"/> to specify an error message.
+    /// </summary>
+    /// <param name="getString">The function used to get user input. Defaults to Console.ReadLine. (optional)</param>
+    /// <param name="write">The function used to output errors. Defaults to Console.WriteLine. (optional)</param>
+    /// <param name="message">The error message sent if something other than y or n is inputted. (optional)</param>
+    /// <returns>Once a valid input is entered: <see langword="true"/> if input was "y", <see langword="false"/> if input was "n".</returns>
+    public static bool GetYNStrict(Func<string> getString = null, Action<string> write = null, string message = "Invalid format... use y or n.")
+    {
+        getString ??= Console.ReadLine;
+        write ??= Console.WriteLine;
+
+        while (true)
+        {
+            switch (getString().Trim().ToLower())
+            {
+                case "y":
+                    return true;
+                case "n":
+                    return false;
+                default:
+                    write(message);
+                    continue;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Prompts for input until it receives a string contained in <paramref name="options"/>. Can optionally use parameter <paramref name="message"/> to specify an error message.
+    /// </summary>
+    /// <param name="options">The collection of valid options.</param>
+    /// <param name="getString">The function used to get user input. Defaults to Console.ReadLine. (optional)</param>
+    /// <param name="write">The function used to output errors. Defaults to Console.WriteLine. (optional)</param>
+    /// <param name="message">The error message sent if a string not contained in <paramref name="options"/> is inputted. (optional)</param>
+    /// <returns>The first string entered contained within <paramref name="options"/></returns>
+    public static string GetOption(IEnumerable<string> options, Func<string> getString = null, Action<string> write = null, string message = "Value inputted is not a valid option.")
+    {
+        getString ??= Console.ReadLine;
+        write ??= Console.WriteLine;
+
+        while (true)
+        {
+            string s = getString();
+            if (options.Contains(s)) return s;
+            else write(message);
+        }
+    }
+
+    /// <summary>
+    /// Prompts for input until it receives a string contained in <paramref name="options"/>. Can optionally use parameter <paramref name="message"/> to specify an error message.
+    /// </summary>
+    /// <param name="options">The collection of valid options.</param>
+    /// <param name="getString">The function used to get user input. Defaults to Console.ReadLine. (optional)</param>
+    /// <param name="write">The function used to output errors. Defaults to Console.WriteLine. (optional)</param>
+    /// <param name="message">The error message sent if a string not contained in <paramref name="options"/> is inputted. (optional)</param>
+    /// <returns>The first string entered contained within <paramref name="options"/></returns>
+    public static T GetOption<T>(IEnumerable<string> options, Func<string, T> convert, Func<string> getString = null, Action<string> write = null, string message = "Value inputted is not a valid option.")
+    {
+        getString ??= Console.ReadLine;
+        write ??= Console.WriteLine;
+
+        while (true)
+        {
+            string s = getString();
+            if (options.Contains(s)) return convert(s);
+            else write(message);
+        }
+    }
+
+    /// <summary>
+    /// Prompts for input until it receives a string contained as a key within <paramref name="options"/>, then returns the corresponding value within <paramref name="options"/>. Can optionally use parameter <paramref name="message"/> to specify an error message.
+    /// </summary>
+    /// <param name="options">The dictionary of valid options.</param>
+    /// <param name="getString">The function used to get user input. Defaults to Console.ReadLine. (optional)</param>
+    /// <param name="write">The function used to output errors. Defaults to Console.WriteLine. (optional)</param>
+    /// <param name="message">The error message sent if a string not contained in <paramref name="options"/> is inputted. (optional)</param>
+    /// <returns>The first string entered contained within <paramref name="options"/></returns>
+    public static T GetOption<T>(Dictionary<string, T> options, Func<string> getString = null, Action<string> write = null, string message = "Value inputted is not a valid option.")
+    {
+        getString ??= Console.ReadLine;
+        write ??= Console.WriteLine;
+
+        while (true)
+        {
+            string s = getString();
+            if (options.ContainsKey(s)) return options[s];
+            else write(message);
+        }
+    }
+}
---
+++ InputHandler/InputHandler_Until.cs
@@ -0,0 +1,253 @@
+﻿namespace InputHandler;
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+
+/// <summary>
+/// Contains a number of methods for handling user input.
+/// </summary>
+public static partial class Input
+{
+    /// <summary>
+    /// Takes input until a <see langword="null"/>, empty, or whitespace string is sent.
+    /// </summary>
+    /// <param name="getString">The function used to get user input. Defaults to Console.ReadLine. (optional)</param>
+    /// <returns>A string representing all input received excluding the final <see langword="null"/>, empty, or whitespace string.</returns>
+    public static string GetUntilWhiteSpace(Func<string> getString = null)
+    {
+        getString ??= Console.ReadLine;
+
+        StringBuilder stringBuilder = new();
+        while (true)
+        {
+            string input = getString();
+            if (string.IsNullOrWhiteSpace(input)) return stringBuilder.ToString();
+            else stringBuilder.AppendLine(input);
+        }
+    }
+
+    /// <summary>
+    /// Takes input until a <see langword="null"/> or empty string is sent.
+    /// </summary>
+    /// <param name="getString">The function used to get user input. Defaults to Console.ReadLine. (optional)</param>
+    /// <returns>A string representing all input received excluding the final <see langword="null"/>, empty, or whitespace string.</returns>
+    public static string GetUntilEmpty(Func<string> getString = null)
+    {
+        getString ??= Console.ReadLine;
+
+        StringBuilder stringBuilder = new();
+        while (true)
+        {
+            string input = getString();
+            if (string.IsNullOrEmpty(input)) return stringBuilder.ToString();
+            else stringBuilder.AppendLine(input);
+        }
+    }
+
+    /// <summary>
+    /// Takes input until <paramref name="shouldStop"/> returns <see langword="true"/>.
+    /// </summary>
+    /// <param name="shouldStop">The predicate that checks input to determine whether or not to stop taking input. If this returns true, the method will stop taking input.</param>
+    /// <param name="getString">The function used to get user input. Defaults to Console.ReadLine. (optional)</param>
+    /// <param name="includeFinal">Whether or not to include the final (breaking) input. (optional)</param>
+    /// <returns>A string representing all input received, including the final (breaking) input if <param name="includeFinal"> is <see langword="null">.</returns>
+    public static string GetUntil(Predicate<string> shouldStop, Func<string> getString = null, bool includeFinal = false)
+    {
+        getString ??= Console.ReadLine;
+
+        StringBuilder stringBuilder = new();
+        while (true)
+        {
+            string input = getString();
+            if (shouldStop(input))
+            {
+                if (includeFinal) stringBuilder.AppendLine(input);
+                return stringBuilder.ToString();
+            }
+            else stringBuilder.AppendLine(input);
+        }
+    }
+
+    /// <summary>
+    /// Takes input until a <see langword="null"/>, empty, or whitespace string is sent.
+    /// </summary>
+    /// <param name="getString">The function used to get user input. Defaults to Console.ReadLine. (optional)</param>
+    /// <returns>A list of strings representing all input received excluding the final <see langword="null"/>, empty, or whitespace string.</returns>
+    public static List<string> ListUntilWhiteSpace(Func<string> getString = null)
+    {
+        getString ??= Console.ReadLine;
+
+        List<string> list = new();
+        while (true)
+        {
+            string input = getString();
+            if (string.IsNullOrWhiteSpace(input)) return list;
+            else list.Add(input);
+        }
+    }
+
+    /// <summary>
+    /// Takes input until a <see langword="null"/>, empty, or whitespace string is sent.
+    /// </summary>
+    /// <param name="convert">The function that converts user input into type <typeparamref name="T"/></param>
+    /// <param name="getString">The function used to get user input. Defaults to Console.ReadLine. (optional)</param>
+    /// <returns>A list of <typeparamref name="T"/> representing all input received excluding the final <see langword="null"/>, empty, or whitespace string.</returns>
+    public static List<T> ListUntilWhiteSpace<T>(Func<string, T> convert, Func<string> getString = null)
+    {
+        getString ??= Console.ReadLine;
+
+        List<T> list = new();
+        while (true)
+        {
+            string input = getString();
+            if (string.IsNullOrWhiteSpace(input)) return list;
+            else list.Add(convert(input));
+        }
+    }
+
+    /// <summary>
+    /// Takes input until a <see langword="null"/> or empty string is sent.
+    /// </summary>
+    /// <param name="getString">The function used to get user input. Defaults to Console.ReadLine. (optional)</param>
+    /// <returns>A list of strings representing all input received excluding the final <see langword="null"/>, empty, or whitespace string.</returns>
+    public static List<string> ListUntilEmpty(Func<string> getString = null)
+    {
+        getString ??= Console.ReadLine;
+
+        List<string> list = new();
+        while (true)
+        {
+            string input = getString();
+            if (string.IsNullOrEmpty(input)) return list;
+            else list.Add(input);
+        }
+    }
+
+    /// <summary>
+    /// Takes input until a <see langword="null"/> or empty string is sent.
+    /// </summary>
+    /// <param name="convert">The function that converts user input into type <typeparamref name="T"/></param>
+    /// <param name="getString">The function used to get user input. Defaults to Console.ReadLine. (optional)</param>
+    /// <returns>A list of <typeparamref name="T"/> representing all input received excluding the final <see langword="null"/>, empty, or whitespace string.</returns>
+    public static List<T> ListUntilEmpty<T>(Func<string, T> convert, Func<string> getString = null)
+    {
+        getString ??= Console.ReadLine;
+
+        List<T> list = new();
+        while (true)
+        {
+            string input = getString();
+            if (string.IsNullOrEmpty(input)) return list;
+            else list.Add(convert(input));
+        }
+    }
+
+    /// <summary>
+    /// Takes input until <paramref name="shouldStop"/> returns <see langword="true"/>.
+    /// </summary>
+    /// <param name="shouldStop">The predicate that checks input to determine whether or not to stop taking input. If this returns true, the method will stop taking input.</param>
+    /// <param name="getString">The function used to get user input. Defaults to Console.ReadLine. (optional)</param>
+    /// <param name="includeFinal">Whether or not to include the final (breaking) input. (optional)</param>
+    /// <returns>A list of strings representing all input received including the final (breaking) input if <paramref name="includeFinal"/> is <see langword="true"/>.</returns>
+    public static List<string> ListUntil(Predicate<string> shouldStop, Func<string> getString = null, bool includeFinal = false)
+    {
+        getString ??= Console.ReadLine;
+
+        List<string> list = new();
+        while (true)
+        {
+            string input = getString();
+            if (shouldStop(input))
+            {
+                if (includeFinal) list.Add(input);
+                return list;
+            }
+            else list.Add(input);
+        }
+    }
+
+    /// <summary>
+    /// Takes input until <paramref name="shouldStop"/> returns <see langword="true"/>.
+    /// </summary>
+    /// <param name="shouldStop">The predicate that checks input to determine whether or not to stop taking input. If this returns true, the method will stop taking input.</param>
+    /// <param name="convert">The function that converts user input into type <typeparamref name="T"/></param>
+    /// <param name="getString">The function used to get user input. Defaults to Console.ReadLine. (optional)</param>
+    /// <param name="includeFinal">Whether or not to include the final (breaking) input. (optional)</param>
+    /// <returns>A list of <typeparamref name="T"/> representing all input received including the final (breaking) input if <paramref name="includeFinal"/> is <see langword="true"/>.</returns>
+    public static List<T> ListUntil<T>(Predicate<string> shouldStop, Func<string, T> convert, Func<string> getString = null, bool includeFinal = false)
+    {
+        getString ??= Console.ReadLine;
+
+        List<T> list = new();
+        while (true)
+        {
+            string input = getString();
+            if (shouldStop(input))
+            {
+                if (includeFinal) list.Add(convert(input));
+                return list;
+            }
+            else list.Add(convert(input));
+        }
+    }
+
+    /// <summary>
+    /// Yield-returns consecutive inputs until a <see langword="null"/>, empty, or whitespace string is sent.
+    /// </summary>
+    /// <param name="getString">The function used to get user input. Defaults to Console.ReadLine. (optional)</param>
+    /// <returns>Each consecutive input entered excluding the final (breaking) input.</returns>
+    public static IEnumerable<string> YieldUntilWhiteSpace(Func<string> getString = null)
+    {
+        getString ??= Console.ReadLine;
+
+        while (true)
+        {
+            string input = getString();
+            if (string.IsNullOrWhiteSpace(input)) break;
+            yield return input;
+        }
+    }
+
+    /// <summary>
+    /// Yield-returns consecutive inputs until a <see langword="null"/> or empty string is sent.
+    /// </summary>
+    /// <param name="getString">The function used to get user input. Defaults to Console.ReadLine. (optional)</param>
+    /// <returns>Each consecutive input entered excluding the final (breaking) input.</returns>
+    public static IEnumerable<string> YieldUntilEmpty(Func<string> getString = null)
+    {
+        getString ??= Console.ReadLine;
+
+        while (true)
+        {
+            string input = getString();
+            if (string.IsNullOrEmpty(input)) break;
+            yield return input;
+        }
+    }
+
+    /// <summary>
+    /// Yield-returns consecutive inputs until <paramref name="shouldStop"/> returns <see langword="true"/>.
+    /// </summary>
+    /// <param name="shouldStop">The predicate that checks input to determine whether or not to stop taking input. If this returns true, the method will stop taking input.</param>
+    /// <param name="getString">The function used to get user input. Defaults to Console.ReadLine. (optional)</param>
+    /// <param name="includeFinal">Whether or not to include the final (breaking) input. (optional)</param>
+    /// <returns>Each consecutive input entered, including the final (breaking) input if <paramref name="shouldStop"/> is <see langword="true"/></returns>
+    public static IEnumerable<string> YieldUntil(Predicate<string> shouldStop, Func<string> getString = null, bool includeFinal = false)
+    {
+        getString ??= Console.ReadLine;
+
+        while (true)
+        {
+            string input = getString();
+            if (shouldStop(input))
+            {
+                if (includeFinal) yield return input;
+                break;
+            }
+
+            yield return input;
+        }
+    }
+}
